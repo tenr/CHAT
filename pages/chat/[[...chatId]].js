6def67e9bc9c1d4868b15e801889b700372d8bcd
tenr <grodriguez1224@gmail.com>
2023-06-04 13:09:45 -0400

    got the stream in

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,7 +1,32 @@
-import { ChatSidebar } from "components/ChatSidebar";
+import { ChatSidebar } from "../../components/ChatSidebar/ChatsSdebar";
 import Head from "next/head";
+import { streamReader } from "openai-edge-stream";
+import { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+  const [messageText, setMessageText] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(messageText);
+    const response = await fetch(`/api/chat/sendMessage`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ message: messageText }),
+    });
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getreader();
+    await streamReader(reader, (message) => {
+      console.log("MESSAGE:", message);
+    });
+  };
+
   return (
     <>
       <Head>
@@ -11,7 +36,21 @@ export default function Home() {
         <ChatSidebar />
         <div className=" flex flex-col bg-gray-700">
           <div className="flex-1">chat window</div>
-          <footer className="bg-gray-800 p-10">chat footer</footer>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                  value={messageText}
+                  onChange={(e) => setMessageText(e.target.value)}
+                  className=" h-14 w-full resize-none overflow-auto rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
+                  placeholder="Send a message..."
+                />
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </>
